6531eceaf8878770fbd0c7acfcadb3a3bd5c1637
FuAmin <627020290@qq.com>
2018-09-29 21:36:36 +0800

    second

--- testGit/src/testGit/testEclipseGit.java
+++ testGit/src/testGit/testEclipseGit.java
@@ -3,7 +3,9 @@ package testGit;
 public class testEclipseGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/**
+		 *amin    
+		 */
 		System.out.println("aaaaaaaaaaaa");
 	}
 
