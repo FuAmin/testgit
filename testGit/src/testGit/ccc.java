c063460d92c2ae89141bf892fbd1b62c1db1131f
FuAmin <627020290@qq.com>
2018-09-29 22:10:37 +0800

    add

--- testGit/src/testGit/ccc.java
+++ testGit/src/testGit/ccc.java
@@ -5,6 +5,7 @@ public class ccc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi当红四好的撒cccccccc");
+		System.out.println("leellele");
 	}
 
 }
